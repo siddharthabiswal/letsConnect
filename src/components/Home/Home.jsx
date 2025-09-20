a453b3131a5eefefa39496d5ebf01ab5fac369c9
siddharthabiswal <siddharthabiswal1994@gmail.com>
2025-09-20 14:46:26 +0530

    code refactor

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,39 +1,21 @@
 import { useState } from "react";
 import Header from "../Header/Header";
 import SideMenu from "../SideMenu/SideMenu";
-
 import ImageCarousel from "../Carousel/ImageCarousel";
 import ServicesDisplay from "../ServicesDisplay/ServicesDisplay";
-
 import "./Home.css";
-import ContactList from "../ContactList/ContactList";
-// ContactList
-// import RegistrationForm from "../RegistrationForm/RegistrationForm";
-// import DropDownMenuCheckBox from "../Common/DropDownMenuCheckBox/DropDownMenuCheckBox";
-//import AdvertisementsPopUp from "../AdvertisementsPopUp/AdvertisementsPopUp";
-//import UserForm from "../DataEntryDashboard/DataEntryDashboard";
-//import FetchUsers from "../DataShowDashboard/DataShowDashboard";
 
 const Home = () => {
-    const [menuOpen, setMenuOpen] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
 
-    return (
-        <div className="HomeContainer">
-
-            <Header toggleMenu={() => setMenuOpen(!menuOpen)} />
-            <ImageCarousel />
-
-            <SideMenu isOpen={menuOpen} onClose={() => setMenuOpen(false)} />
-
-            <ServicesDisplay />
-            <ContactList />
-            {/* <DropDownMenuCheckBox /> */}
-
-            {/* <AdvertisementsPopUp /> */}
-            {/* <UserForm />
-            <FetchUsers /> */}
-        </div>
-    );
+  return (
+    <div className="HomeContainer">
+      <Header toggleMenu={() => setMenuOpen(!menuOpen)} />
+      <ImageCarousel />
+      <SideMenu isOpen={menuOpen} onClose={() => setMenuOpen(false)} />
+      <ServicesDisplay />
+    </div>
+  );
 };
 
 export default Home;
